540f939e70b6894949c82beb28bea3b75a2a53ca
Nipuni Hansika <kanipunihansika@gmail.com>
2023-03-25 19:58:05 +0800

    commit, Aligning PointCloud using Script

---
+++ Assets/Scripts/NewVertexCreator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace pointcloud_nreal
+{
+    class NewVertexCreator
+    {
+        private StreamWriter verticesWriter;
+        List<Vector3> verticesData;
+        Matrix4x4 matrix4x4;
+
+        public List<Vector3> createVerticesData(List<Vector3> verticesData)
+        {
+            verticesWriter = new StreamWriter("./Assets/PLYFiles/verticesWriter.txt", true);   //**Windows path
+            //verticesWriter = new StreamWriter("/storage/emulated/0/Nreal/verticesWriter.txt");     //**Nral path
+
+            int vertexCount = verticesData.Count();
+            List<Vector3> verticesDataTemp = new List<Vector3>(vertexCount);
+            //Debug.Log("Vertices  #################### " + vertexCount); 
+            Vector4 column0 = new Vector4(1f, 0f, 0f, 0f);
+            Vector4 column1 = new Vector4(0f, 1f, 0f, 0f);
+            Vector4 column2 = new Vector4(0f, 0f, 1f, 0f);
+            Vector4 column3 = new Vector4(-1f, 1f, 5f, 1f);
+            matrix4x4 = new Matrix4x4(column0, column1, column2, column3); 
+
+            //for (int n = 0; n < 5; n++)
+            foreach (var x in verticesData)
+            {
+                Vector3 vertexTemp = new Vector3(0f, 0f, 0f);
+                vertexTemp = multiplyVerticesWithMatrix4x4(matrix4x4, x);
+                verticesDataTemp.Add(vertexTemp);
+                //verticesWriter.WriteLine(point4f.ToString());
+            }
+            verticesData = verticesDataTemp;
+            foreach (var x in verticesData)
+            {
+                //Debug.Log(x.ToString());
+                verticesWriter.WriteLine(x.ToString());
+            }
+            verticesWriter.Flush();
+            verticesWriter.Close();
+
+            return verticesData;
+        }
+
+        private Vector3 multiplyVerticesWithMatrix4x4(Matrix4x4 matrix4x4, Vector3 vertex)
+        {
+            Vector3 vertexNew = matrix4x4.MultiplyPoint(vertex);
+            return vertexNew;
+        }
+    }
+}
--- Assets/Scripts/TestDataImporter.cs
+++ Assets/Scripts/TestDataImporter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace pointcloud_nreal
@@ -9,15 +10,79 @@ namespace pointcloud_nreal
 
         void Start()
         {
-            var importer = new ImporterPLY();
-            //List<Vector3> vertices = importer.GetVerticesOfPointcloud("./Assets/PLYFiles/Meeting_room_4010_LiDAR_binary.ply");              //**Windows path
-            //List<Color32> colors = importer.GetColorsOfPointcloud("./Assets/PLYFiles/Meeting_room_4010_LiDAR_binary.ply");                  //**Windows path
-            //Mesh mesh = importer.ImportAsMesh("./Assets/PLYFiles/Meeting_room_4010_LiDAR_binary.ply", vertices, colors);                    //**Windows path
-            List<Vector3> vertices = importer.GetVerticesOfPointcloud("/storage/emulated/0/Nreal/Meeting_room_4010_LiDAR_binary.ply");     //**Nral path
-            List<Color32> colors = importer.GetColorsOfPointcloud("/storage/emulated/0/Nreal/Meeting_room_4010_LiDAR_binary.ply");         //**Nral path   
-            Mesh mesh = importer.ImportAsMesh("/storage/emulated/0/Nreal/Meeting_room_4010_LiDAR_binary.ply", vertices, colors);           //**Nral path
-            var meshfilter = pointCloud.GetComponent<MeshFilter>();
-            meshfilter.mesh = mesh;
+            importAndAlignPointCloud();
         }
+
+        public void importPointCloud()
+        {
+            try
+            {
+                var importer = new ImporterPLY();
+
+                //List<Vector3> vertices = importer.GetVerticesOfPointcloud("./Assets/PLYFiles/Meeting_room_4010_LiDAR_binary.ply");              //**Windows path
+                //List<Color32> colors = importer.GetColorsOfPointcloud("./Assets/PLYFiles/Meeting_room_4010_LiDAR_binary.ply");                  //**Windows path
+                //Mesh mesh = importer.ImportAsMesh("./Assets/PLYFiles/Meeting_room_4010_LiDAR_binary.ply", vertices, colors);                    //**Windows path
+
+                List<Vector3> vertices = importer.GetVerticesOfPointcloud("/storage/emulated/0/Nreal/Meeting_room_4010_LiDAR_binary.ply");     //**Nral path
+                List<Color32> colors = importer.GetColorsOfPointcloud("/storage/emulated/0/Nreal/Meeting_room_4010_LiDAR_binary.ply");         //**Nral path   
+                Mesh mesh = importer.ImportAsMesh("/storage/emulated/0/Nreal/Meeting_room_4010_LiDAR_binary.ply", vertices, colors);           //**Nral path
+
+                var meshfilter = pointCloud.GetComponent<MeshFilter>();
+                meshfilter.mesh = mesh;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed importing: " + e.Message);
+            }
+        }
+
+        public void importAndAlignPointCloud()
+        {
+            try
+            {
+                var importer = new ImporterPLY();
+
+                List<Vector3> vertices = importer.GetVerticesOfPointcloud("./Assets/PLYFiles/Table.ply");              //**Windows path
+                List<Color32> colors = importer.GetColorsOfPointcloud("./Assets/PLYFiles/Table.ply");                  //**Windows path
+
+                //List<Vector3> vertices = importer.GetVerticesOfPointcloud("/storage/emulated/0/Nreal/Table.ply");     //**Nral path
+                //List<Color32> colors = importer.GetColorsOfPointcloud("/storage/emulated/0/Nreal/Table.ply");         //**Nral path 
+
+                var newVertexCreator = new NewVertexCreator();
+                var newVertices = newVertexCreator.createVerticesData(vertices);
+
+                Mesh mesh = importer.ImportAsMesh("./Assets/PLYFiles/Table.ply", newVertices, colors);                    //**Windows path
+                //Mesh mesh = importer.ImportAsMesh("/storage/emulated/0/Nreal/Table.ply", newVertices, colors);           //**Nral path
+
+                var meshfilter = pointCloud.GetComponent<MeshFilter>();
+                meshfilter.mesh = mesh;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed importing: " + e.Message);
+            }
+        }
+
+        public void testVertices()
+        {
+            try
+            {
+                List<Vector3> TestVerticesData = new List<Vector3> {
+                new Vector3(0.4f, -1.3f, -1.6f),
+                new Vector3(0.5f, -1.3f, -1.6f),
+                new Vector3(0.6f, -1.3f, -1.5f),
+                new Vector3(0.7f, -1.3f, -1.5f),
+                new Vector3(1.2f, -0.6f, -0.8f),
+                new Vector3(0.6f, -0.9f, -0.4f),
+                };
+                var newVertexCreator = new NewVertexCreator();
+                var newVertices = newVertexCreator.createVerticesData(TestVerticesData);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed importing: " + e.Message);
+            }
+        }
+
     }
 }
